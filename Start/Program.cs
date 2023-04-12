d3691795e1cd1b1fde140ee453a1c3bf00114ee3
jaeil777 <130571361+jaeil777@users.noreply.github.com>
2023-04-13 01:22:28 +0900

    초기세팅

---
+++ Start/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        bool boolVariable = true;
+        int intVariable = 10;
+        float floatVariable = 3.4f;
+        char charVariable = 'a';
+
+        Console.WriteLine(boolVariable);
+        Console.WriteLine(intVariable);
+        Console.WriteLine(floatVariable);
+        Console.WriteLine(charVariable);
+    }
+}
