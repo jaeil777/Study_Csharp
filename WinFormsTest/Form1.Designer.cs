82d774f1ac1a8f6f2af5f974d0eeb7974d2a2388
jaeil777 <130571361+jaeil777@users.noreply.github.com>
2023-04-14 00:11:39 +0900

    hello world 예제

--- WinFormsTest/Form1.Designer.cs
+++ WinFormsTest/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("빙그레체", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(248, 256);
+            button1.Name = "button1";
+            button1.Size = new Size(264, 103);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("여기어때 잘난체", 48F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(267, 139);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 64);
+            label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
     }
 }
--- WinFormsTest/Form1.cs
+++ WinFormsTest/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World";
+        }
     }
 }
